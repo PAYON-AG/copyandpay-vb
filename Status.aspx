259079c262f9c5a2b2b163e9f4380abea8508eb5
Steve Nester <steve@sahfor.com>
2015-03-13 05:19:47 -0700

    COPYandPAY VB

---
+++ Status.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Status.aspx.vb" Inherits="Status" %>
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>COPYandPAY C# - Status</title>
+</head>
+<body>
+    <div><%=Result%></div>
+</body>
+</html>
---
+++ Status.aspx.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Net
+Imports System.IO
+
+Partial Class Status
+    Inherits System.Web.UI.Page
+    Public Result As String = ""
+
+    Protected Sub Page_PreRender(sender As Object, e As System.EventArgs) Handles Me.PreRender
+        getPaymentStatus(Request.Form("id"))
+    End Sub
+
+    Private Sub getPaymentStatus(checkoutId As String)
+        Dim url As String = "https://test.oppwa.com/v1/checkouts/" + checkoutId + "/payment"
+        Dim request As WebRequest = WebRequest.Create(url)
+        Dim webresponse As WebResponse = request.GetResponse()
+        Dim dataStream As Stream = webresponse.GetResponseStream()
+        Dim reader As New StreamReader(dataStream)
+        Dim response As String = reader.ReadToEnd()
+        reader.Close()
+        dataStream.Close()
+        webresponse.Close()
+        Dim jss As New System.Web.Script.Serialization.JavaScriptSerializer()
+        Dim dict As Dictionary(Of String, Object) = jss.Deserialize(Of Dictionary(Of String, Object))(response)
+
+        If dict("result")("code").StartsWith("000") Then
+            Result = "SUCCESS <br/><br/> Here is the result of your transaction: <br/><br/>"
+            Result += response
+        Else
+            Result = "ERROR <br/><br/> Here is the result of your transaction: <br/><br/>"
+            Result += response
+        End If
+    End Sub
+End Class
